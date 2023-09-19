a8f88d407696558d2a7027e853e2750c47fec7e8
Javier Flores <jffc.dev@gmail.com>
2023-09-19 09:50:24 -0500

    feat: divide word index page into modular components

--- pages/words/index.tsx
+++ pages/words/index.tsx
@@ -5,24 +5,40 @@ import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
 
 import React, { useState, useEffect } from 'react';
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
 
-interface Word {
-    id: string;
-    content: string;
-}
+import { Word } from "../../types/words/Word";
+import FormWordMaintenanceList from "../../components/words/maintenance/FormWordMaintenanceList";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import getAllWords from "../../services/words/getAllWords";
+import FormWordMaintenanceToolbar from "../../components/words/maintenance/FormWordMaintenanceToolbar";
+import FormWordMaintenanceDialog from "../../components/words/maintenance/FormWordMaintenanceDialog";
 
 export default function WordsPage() {
     const [words, setWords] = useState<Word[]>([]);
+    const [visible, setVisible] = useState<boolean>(false);
+
+    const fetchData = async () => {
+        try {
+            const data = await getAllWords();
+            setWords(data);
+        } catch (error) {
+            console.error("Error getting words:", error);
+        }
+    };
+    
+    useEffect(() => {
+        fetchData();
+    }, []);
+    
 
     return (
-        <div className="card">
-            <DataTable value={words} tableStyle={{ minWidth: '50rem' }}>
-                <Column field="id" header="ID"></Column>
-                <Column field="content" header="Content"></Column>
-            </DataTable>
-        </div>
+        <>
+            <div className="container">
+                <FormWordMaintenanceToolbar selectedProducts={[]} setVisible={setVisible}></FormWordMaintenanceToolbar>
+                <FormWordMaintenanceList setWords={setWords} words={words}></FormWordMaintenanceList>
+            </div>
+            <FormWordMaintenanceDialog visible={visible} setVisible={setVisible} setWords={setWords}></FormWordMaintenanceDialog>
+        </>
     );
 }
   
