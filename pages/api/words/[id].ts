132c58b4acaa1a848612025e6aca02ea936a6450
Javier Flores <jffc.dev@gmail.com>
2023-09-22 10:14:33 -0500

    feat: delete endpoint for api word by id added

--- pages/api/words/[id].ts
+++ pages/api/words/[id].ts
@@ -2,7 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { Word } from '../../../types/words/Word';
 import axios from 'axios';
  
-type ResponseData = Word[]
+type ResponseData = Word
  
 export default async function handler(
   req: NextApiRequest,
@@ -11,10 +11,17 @@ export default async function handler(
 
   if (req.method === 'PUT') {
 
-    const words: Word[] = await axios.put(`http://localhost:8080/words/${req.body.id}`, req.body).then(resp => {
+    const words: Word = await axios.put(`http://localhost:8080/words/${req.body.id}`, req.body).then(resp => {
       return resp.data;
     });
     res.status(200).json(words)
+
+  } else if (req.method === 'DELETE') {
+
+    const idWord: number = await axios.delete(`http://localhost:8080/words/${req.query.id}`).then(resp => {
+      return resp.data;
+    });
+    res.status(200).json({id: idWord})
 
   } else {
     // Handle any other HTTP method
