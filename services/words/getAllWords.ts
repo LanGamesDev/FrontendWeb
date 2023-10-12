3afdaaee05df707bcd3516b88fb3310f5a3df328
Javier Flores <jffc.dev@gmail.com>
2023-10-11 23:48:32 -0500

    feat: get words service response improved

--- services/words/getAllWords.ts
+++ services/words/getAllWords.ts
@@ -1,16 +1,21 @@
 import axios from "axios";
 import { Word } from "../../types/words/Word";
+import { MessageService } from "../../types/general/MessageService";
+import { MSG_TYPE_ERROR, MSG_TYPE_SUCCESS } from "../../constants/general/ConstantsRoutes";
 
-const getAllWords = async(): Promise<Word[]> => {
+const getAllWords = async(): Promise<MessageService> => {
     try {
         const words: Word[] = await axios.get('http://localhost:3001/api/words').then(resp => {
             return resp.data;
+        })
+        .catch(error => {
+            throw error;
         });
 
-        return words;
+        return {type: MSG_TYPE_SUCCESS, data: words};
     } catch (error) {
         console.error("An error occurred while fetching the words: ", error);
-        throw error;
+        return {type: MSG_TYPE_ERROR, data: null};
     }
 }
 
