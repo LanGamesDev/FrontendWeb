924edde4201849e784ea617d1fd736ecde470a1f
Javier Flores <jffc.dev@gmail.com>
2023-10-15 14:59:55 -0500

    feat: constant for edit and new state added

--- constants/general/ConstantsRoutes.ts
+++ constants/general/ConstantsRoutes.ts
@@ -3,3 +3,7 @@ export const ROUTE_API_LOCAL = "http://localhost:3001/api/";
 //Response types
 export const MSG_TYPE_SUCCESS: number = 1
 export const MSG_TYPE_ERROR: number = 0
+
+//State types
+export const STATE_TYPE_NEW: number = 0
+export const STATE_TYPE_EDIT: number = 1
