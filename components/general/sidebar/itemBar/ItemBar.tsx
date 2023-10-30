243cdd4a5f89c1609cdeaaa25531a9e588efe214
Javier Flores <jffc.dev@gmail.com>
2023-10-29 19:20:17 -0500

    feat: ItemBar component improved

--- components/general/sidebar/itemBar/ItemBar.tsx
+++ components/general/sidebar/itemBar/ItemBar.tsx
@@ -1,15 +1,49 @@
 import Link from "next/link";
+import { SideBarMenu } from "../../../../types/general/SideBarMenu";
+import { useState } from 'react';
 
-const ItemBar = ({}) => {
+interface ItemBarProps {
+    item: SideBarMenu
+}
+const ItemBar: React.FC<ItemBarProps> = ({item}) => {
+
+    const [active, setActive] = useState(false);
+
+    const handleActive = () => {
+        setActive(oldState => !oldState)
+    }
 
     return (
-        <li>
-            <Link href="/words">
-                <i className='pi pi-th-large' ></i>
-                <span className="link_name">Words</span>
-            </Link>
-            <ul className="sub-menu blank">
-                <li><Link className="link_name" href="/words">Words</Link></li>
+        <li className={`${(active && !item.url) ? "showMenu" : ""}`} onClick={handleActive}>
+            {
+            (item.url) ?
+                <Link href={item.url!}>
+                    <i className={item.icon} ></i>
+                    <span className="link_name">{item.name}</span>
+                </Link>
+            :
+                <div className="iocn-link">
+                    <div>
+                        <i className={item.icon} ></i>
+                        <span className="link_name">{item.name}</span>
+                    </div>
+                    <i className='pi pi-chevron-down arrow' ></i>
+                </div>
+            } 
+
+            <ul className="sub-menu">
+                <li>
+                {
+                (item.url) ?
+                    <Link className="link_name" href={item.url!}>{item.name}</Link>
+                :
+                    <div className="link_name">{item.name}</div>
+                }
+                </li>
+
+                {item.items.map((item, index) => (
+                    <li key={index}><Link href={item.url}>{item.name}</Link></li>
+                ))}
             </ul>
         </li>
     );
