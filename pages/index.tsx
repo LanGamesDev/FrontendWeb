81d4a46cb5371df48a1907cc4272469f0f6b07e7
Javier Flores <jffc.dev@gmail.com>
2023-10-30 23:21:38 -0500

    feat: IndexPage added

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import LayoutSidebar from "../components/general/layout/LayoutSidebar";
+
+const IndexPage = () => {
+
+    return (
+        <LayoutSidebar>
+            
+        </LayoutSidebar>
+    );
+
+}
+
+export default IndexPage;
