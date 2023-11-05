10722cbbe65429eeb8a5355f395ac57515015ed7
Javier Flores <jffc.dev@gmail.com>
2023-11-05 12:27:41 -0500

    feat: ConstantsForms file added

---
+++ constants/general/ConstantsForms.ts
@@ -0,0 +1,4 @@
+//Translate game states
+export const STATE_TRANSLATEGAME_ANSWERING: number = 0
+export const STATE_TRANSLATEGAME_PREVIEW: number = 1
+export const STATE_TRANSLATEGAME_SENDED: number = 2
