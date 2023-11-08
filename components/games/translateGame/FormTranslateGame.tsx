ec38892c11fb23b8f60e5704b26bdf83d10ad9e0
Javier Flores <jffc.dev@gmail.com>
2023-11-08 06:48:16 -0500

    feat: FormTranslateGame component improved

--- components/games/translateGame/FormTranslateGame.tsx
+++ components/games/translateGame/FormTranslateGame.tsx
@@ -1,17 +1,32 @@
-import { Word } from "../../../types/words/Word";
-import QuestionTranslate from "./QuestionTranslate";
+import { Panel } from "primereact/panel";
+import { Question } from "../../../types/words/Question";
+import { InputText } from "primereact/inputtext";
+import TranslateGameResults from "./TranslateGameResults";
 
 interface FormTranslateGameProps {
-    words: Word[]
+    questions: Question[]
+    currentQuestion: number
+    formik: any
 }
 
-const FormTranslateGame: React.FC<FormTranslateGameProps> = ({words}) => {
+const FormTranslateGame: React.FC<FormTranslateGameProps> = ({questions, currentQuestion, formik}) => {
+    
     return (
-        <div>
-            {words.map((item, index) => (
-                <QuestionTranslate key={index} word={item} index={index}></QuestionTranslate>
-            ))}
-        </div>
+        (questions.length > 0) &&
+        <Panel header={`Question No. ${currentQuestion + 1}`} className="mb-2" toggleable>
+            <div className="d-flex justify-content-between">
+                <div className="d-flex align-items-center" style={{ flex: '1' }}>
+                    <p className="m-0">{questions[currentQuestion].word!.content}</p>
+                </div>
+                <div style={{ flex: '1' }}>
+                    <form onSubmit={formik.handleSubmit} className="flex flex-column gap-2">
+                        <InputText id="answer" value={formik.values?.answer} onChange={(e) => {
+                                    formik.setFieldValue('answer', e.target.value);
+                                }} required autoComplete='off' className={`form-control`} autoFocus={true} />
+                    </form>
+                </div>
+            </div>
+        </Panel>
     )
 }
 
