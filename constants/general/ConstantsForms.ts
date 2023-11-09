3aec99fba9c1a8bde66e928850b9ecb10469e3b8
Javier Flores <jffc.dev@gmail.com>
2023-11-09 05:05:33 -0500

    feat: color results constants added

--- constants/general/ConstantsForms.ts
+++ constants/general/ConstantsForms.ts
@@ -2,3 +2,9 @@
 export const STATE_TRANSLATEGAME_ANSWERING: number = 0
 export const STATE_TRANSLATEGAME_PREVIEW: number = 1
 export const STATE_TRANSLATEGAME_SENDED: number = 2
+
+
+//Results game colors
+export const COLOR_RESULT_DANGER: string = "#E81714"
+export const COLOR_RESULT_WARNING: string = "#E8E814"
+export const COLOR_RESULT_SUCCESS: string = "#22CC00"
