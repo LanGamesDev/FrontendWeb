29161b30f62f7063383e35079f45161724bc7ea3
Javier Flores <jffc.dev@gmail.com>
2023-11-23 03:30:01 -0500

    feat: formatDate function added

--- utils/functions/general/stringsHandle.ts
+++ utils/functions/general/stringsHandle.ts
@@ -24,3 +24,33 @@ export const removeAccents = (str: string): string => {
         return accentMap[letter];
     });
 }
+
+interface DateTimeFormatOptions {
+    localeMatcher?: 'best fit' | 'lookup';
+    weekday?: 'long' | 'short' | 'narrow';
+    era?: 'long' | 'short' | 'narrow';
+    year?: 'numeric' | '2-digit';
+    month?: 'numeric' | '2-digit' | 'long' | 'short' | 'narrow';
+    day?: 'numeric' | '2-digit';
+    hour?: 'numeric' | '2-digit';
+    minute?: 'numeric' | '2-digit';
+    second?: 'numeric' | '2-digit';
+    timeZoneName?: 'long' | 'short';
+    formatMatcher?: 'basic' | 'best fit';
+    hour12?: boolean;
+    timeZone: 'UTC';
+}
+
+export const formatDate = (dateString: string) => {
+    const options: DateTimeFormatOptions = {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false,
+        timeZone: 'UTC'
+    };
+    return new Intl.DateTimeFormat('es-ES', options).format(new Date(dateString));
+};
