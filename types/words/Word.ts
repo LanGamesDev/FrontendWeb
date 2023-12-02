0936da5b806a9630abf02de9f3f151c88099b220
Javier Flores <jffc.dev@gmail.com>
2023-12-02 16:34:33 -0500

    feat: context added to Word

--- types/words/Word.ts
+++ types/words/Word.ts
@@ -3,6 +3,7 @@ import { Translate } from "./Translate";
 export type Word = {
     id?: number;
     content?: string;
+    context?: string;
     translates?: Translate[];
     createdDate?: Date;
     updatedDate?: Date;
